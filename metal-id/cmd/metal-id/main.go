d075cad908b2a36aca7cc322c7a1445f3468d392
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-07-12 16:51:30 +0300

    Prepare scaffolding for adding data sources

--- metal-id/cmd/metal-id/main.go
+++ metal-id/cmd/metal-id/main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
+	"metal_id"
+
+	"crypto"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	var hwid = metal_id.New()
+
+	var err error
+	var data metal_id.DataSource
+	for _, data = range metal_id.Sources() {
+		for {
+			var chunk []byte
+			chunk = data.Next()
+			if len(chunk) == 0 {
+				break
+			}
+			_, err = hwid.Write(chunk)
+			if err != nil {
+				fail("Failed to add data to fingerprint: %v", err)
+			}
+		}
+	}
+
+	var key crypto.Signer
+	key, err = hwid.Key()
+	if err != nil {
+		fail("Failed to generate machine key: %v", err)
+	}
+
+	var output []byte
+	output, err = metal_id.EncodePublicKey(key.Public())
+	if err != nil {
+		fail("Failed to encode public key: %v", err)
+	}
+	fmt.Println(string(output))
+}
+
+func fail(format string, a ...any) {
+	if !strings.HasSuffix(format, "\n") {
+		format += "\n"
+	}
+	_, err := fmt.Fprintf(os.Stderr, format, a...)
+	if err != nil {
+		panic("failed to write to stderr: " + fmt.Sprint(err))
+	}
+	os.Exit(1)
 }
---
+++ metal-id/source.go
@@ -0,0 +1,17 @@
+package metal_id
+
+// Data source provides hardware based inputs for MetalID to uniquely
+// fingerprint current machine
+type DataSource interface {
+	// Multiple data points will be consumed by subsequent Next() calls.
+	// Data points must be sorted using a stable sorting algorithm.
+	//
+	// After the last data point has been consumed all subsequent calls to
+	// Next() must return nil
+	Next() []byte
+}
+
+// Order of data sources matters. Increment idVersion after any changes
+func Sources() []DataSource {
+	return []DataSource{}
+}
