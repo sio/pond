c2dab662ac8ffff312cd2b3821c45f698a66f033
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-08-10 16:54:38 +0300

    Replace encryption stubs in database/

--- secrets/database/value.go
+++ secrets/database/value.go
@@ -1,9 +1,11 @@
 package database
 
+import "secrets/crypto"
+
 func (db *DB) encryptValue(path []string, value []byte) (encrypted []byte, err error) {
-	return nil, nil
+	return crypto.Encrypt(db.key, path, value)
 }
 
 func (db *DB) decryptValue(path []string, encrypted []byte) (value []byte, err error) {
-	return nil, nil
+	return crypto.Decrypt(db.key, path, encrypted)
 }
