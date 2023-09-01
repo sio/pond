8da6432aa3d9e5aaa2aafb79e18dbc68131b5d95
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-09-01 07:09:38 +0000

    Simplify capability validation code

--- secrets/repo/save.go
+++ secrets/repo/save.go
@@ -30,24 +30,15 @@ func (r *Repository) saveCert(cert *ssh.Certificate) (path string, err error) {
 	var isAdmin *bool
 	for p := range cert.Permissions.CriticalOptions {
 		capability := access.Capability(p)
-		switch {
-		case capability.User():
-			if isAdmin == nil {
-				isAdmin = new(bool)
-			}
-			if *isAdmin {
-				return "", fmt.Errorf("mixing user and administrator capabilities in one cert is not supported")
-			}
-		case capability.Admin():
-			if isAdmin == nil {
-				isAdmin = new(bool)
-				*isAdmin = true
-			}
-			if !(*isAdmin) {
-				return "", fmt.Errorf("mixing user and administrator capabilities in one cert is not supported")
-			}
-		default:
+		if !capability.Valid() {
 			return "", fmt.Errorf("invalid capability: %s", capability)
+		}
+		if isAdmin == nil {
+			isAdmin = new(bool)
+			*isAdmin = capability.Admin()
+		}
+		if *isAdmin != capability.Admin() {
+			return "", fmt.Errorf("mixing user and administrator capabilities in one cert is not supported")
 		}
 	}
 	if *isAdmin {
