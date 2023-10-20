1d348f367ba3a5b148f60d9776fffc4a240df596
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-10-20 14:53:01 +0000

    Make ldsocache_test more conventient
    - Do not fail on non-Linux platforms
    - Do not spam test logs with excessive output

--- initramfs/ldd/ldsocache_test.go
+++ initramfs/ldd/ldsocache_test.go
@@ -2,15 +2,26 @@ package ldd
 
 import (
 	"testing"
+
+	"os"
 )
 
 func TestReadLdsoCache(t *testing.T) {
 	cache, err := ldsoCache("")
+	if os.IsNotExist(err) {
+		t.Skip(err)
+	}
 	if err != nil {
 		t.Fatal(err)
 	}
+	var count uint
 	for key, value := range cache {
+		if count > 10 {
+			t.Log("...")
+			break
+		}
 		t.Logf("%s => %s", key, value)
+		count++
 	}
 	t.Logf("ld.so.cache: %d entries", len(cache))
 	if len(cache) == 0 {
