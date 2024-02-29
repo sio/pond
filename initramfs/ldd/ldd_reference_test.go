a054786e6c8679e06ee35df8159cab763386c4f0
Vitaly Potyarkin <sio.wtf@gmail.com>
2024-02-29 14:21:51 +0000

    Test more executable files againt reference implementation

--- initramfs/ldd/ldd_reference_test.go
+++ initramfs/ldd/ldd_reference_test.go
@@ -3,46 +3,82 @@ package ldd
 import (
 	"testing"
 
+	"fmt"
 	"github.com/sio/pond/lib/sandbox"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"sort"
 	"strings"
 )
 
+const (
+	comparisonMaxCount = 100
+)
+
 func TestReference(t *testing.T) {
-	progs := []string{
-		"/bin/ls",
-		"/bin/bash",
-		"/bin/sh",
-		"/bin/mount",
-		"/bin/test",
+	progs := make(map[string]struct{})
+	for _, dir := range []string{"/bin", "/sbin", "/usr/bin", "/usr/sbin"} {
+		err := walk(dir, &progs)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
-	for _, exe := range progs {
-		t.Run(exe, func(t *testing.T) {
-			if _, err := os.Stat(exe); os.IsNotExist(err) {
-				t.Skipf("%s does not exist", exe)
+	var count int
+	for exe := range progs { // automatically randomized by Go runtime
+		if count > comparisonMaxCount {
+			break
+		}
+		if _, err := os.Stat(exe); os.IsNotExist(err) {
+			t.Skipf("%s does not exist", exe)
+		}
+		want, err := sandbox.Ldd(exe)
+		if err != nil {
+			if err.Error() == "exit status 1" {
+				continue
 			}
-			want, err := sandbox.Ldd(exe)
-			if err != nil {
-				t.Fatalf("reference ldd: %v", err)
+			t.Errorf("%s: reference ldd: %v", exe, err)
+			continue
+		}
+		count++
+		got, err := Depends(exe)
+		if err != nil {
+			t.Errorf("%s: our ldd: %v", exe, err)
+			continue
+		}
+		sort.Strings(want)
+		sort.Strings(got)
+		if len(want) != len(got) {
+			t.Errorf("libraries count mismatch: our %d, reference %d", len(got), len(want))
+		}
+		for i, w := range want {
+			if w != got[i] {
+				t.Error("output does not match reference")
+				t.Logf("reference output:\n%s", strings.Join(want, "\n"))
+				t.Logf("actual output:\n%s", strings.Join(got, "\n"))
+				break
 			}
-			got, err := Depends(exe)
-			if err != nil {
-				t.Fatalf("our ldd: %v", err)
-			}
-			sort.Strings(want)
-			sort.Strings(got)
-			if len(want) != len(got) {
-				t.Errorf("libraries count mismatch: our %d, reference %d", len(got), len(want))
-			}
-			for i, w := range want {
-				if w != got[i] {
-					t.Error("output does not match reference")
-					t.Logf("reference output:\n%s", strings.Join(want, "\n"))
-					t.Logf("actual output:\n%s", strings.Join(got, "\n"))
-					t.FailNow()
-				}
-			}
-		})
+		}
 	}
+}
+
+func walk(dir string, exe *map[string]struct{}) error {
+	return fs.WalkDir(os.DirFS(dir), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return nil
+		}
+		fullpath := filepath.Join(dir, path)
+		stat, err := os.Stat(fullpath)
+		if err != nil {
+			return fmt.Errorf("%s: %w", fullpath, err)
+		}
+		if stat.IsDir() {
+			return nil
+		}
+		if stat.Mode()&0b001001001 == 0 { // check executable bit
+			return nil
+		}
+		(*exe)[fullpath] = struct{}{} // add executable path to results
+		return nil
+	})
 }
