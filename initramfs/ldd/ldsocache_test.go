5298888aafaba2e6f3e183a182a0981c52329b9a
Vitaly Potyarkin <sio.wtf@gmail.com>
2024-03-04 08:43:24 +0000

    Save debug artifacts from GHA environment for later inspection

--- initramfs/ldd/ldsocache_test.go
+++ initramfs/ldd/ldsocache_test.go
@@ -3,7 +3,9 @@ package ldd
 import (
 	"testing"
 
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func TestReadLdsoCache(t *testing.T) {
@@ -11,6 +13,22 @@ func TestReadLdsoCache(t *testing.T) {
 	if os.IsNotExist(err) {
 		t.Skip(err)
 	}
+	t.Cleanup(func() {
+		if !t.Failed() {
+			return
+		}
+		artifacts := os.Getenv("ARTIFACTS")
+		if artifacts == "" {
+			t.Log("Not saving debug files, $ARTIFACTS variable is empty")
+			return
+		}
+		err := copyFile("/etc/ld.so.cache", artifacts)
+		if err != nil {
+			t.Errorf("Failed to save ld.so.cache for debugging: %v", err)
+		} else {
+			t.Logf("Saved ld.so.cache for debugging later: %s", artifacts)
+		}
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -27,4 +45,27 @@ func TestReadLdsoCache(t *testing.T) {
 	if len(cache) == 0 {
 		t.FailNow()
 	}
+}
+
+func copyFile(src, destdir string) error {
+	from, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = from.Close() }()
+
+	err = os.MkdirAll(destdir, 0700)
+	if err != nil {
+		return err
+	}
+
+	dest := filepath.Join(destdir, filepath.Base(src))
+	to, err := os.OpenFile(dest, os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = to.Close() }()
+
+	_, err = io.Copy(to, from)
+	return err
 }
